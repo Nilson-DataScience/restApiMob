76f8d49d0b38ec44364f9be804ddfc2458ed671f
Nilson <nilsongithub@gmail.com>
2025-11-20 13:19:36 -0300

    update

--- api/index.go
+++ api/index.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 
 	. "github.com/tbxark/g4vercel"
 )
@@ -15,6 +17,27 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			"message": "hello go x from vercel !!!!",
 		})
 	})
+
+	server.GET("/mob", func(context *Context) {
+		data, err := os.ReadFile("db.json")
+		if err != nil {
+			context.JSON(500, H{
+				"error": "MENSAGEM DE ERRO ATUALIZADA: Arquivo não encontrado!",
+			})
+			return
+		}
+
+		var jsonData map[string]interface{}
+		if err := json.Unmarshal(data, &jsonData); err != nil {
+			context.JSON(500, H{
+				"error": "Erro ao decodificar JSON!",
+			})
+			return
+		}
+
+		context.JSON(200, jsonData)
+	})
+
 	server.GET("/hello", func(context *Context) {
 		name := context.Query("name")
 		if name == "" {
